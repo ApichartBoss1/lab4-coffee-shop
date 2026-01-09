25279ebd0e5aad0b796133faea4806eb3d3971bf
aphboss <apichart91247@gmail.com>
2026-01-09 10:48:45 +0700

    Update frontend coffee menu

---
+++ client/src/components/Menus/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Get All Menus</h1>
+    <p><button @click="goCreate">+ เพิ่มเมนูกาแฟ</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goCreate() {
+      this.$router.push('/menu/create')
+    }
+  }
+}
+</script>
+
+<style scoped></style>
